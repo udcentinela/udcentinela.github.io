bbebdd1f1eec8f9082c3b1c10e6109aa028716ea
UD Centinela <ud.centinela@gmail.com>
2026-08-21 15:47:29 +0000

    feat(brand): integrar escudos oficiales actualizados (azul y dorado) con Deportivo Centinela

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 /**
  * UD Centinela - Service Worker PWA & Modo Estadio Offline
- * Version: 20260821_revert_v1
+ * Version: 20260821_brand_final_v1
  */
 
-const CACHE_NAME = 'udc-cache-v20260821_revert_v1';
-const DATA_CACHE_NAME = 'udc-data-cache-v20260821_revert_v1';
+const CACHE_NAME = 'udc-cache-v20260821_brand_final_v1';
+const DATA_CACHE_NAME = 'udc-data-cache-v20260821_brand_final_v1';
 
 const PRECACHE_ASSETS = [
   '/',
