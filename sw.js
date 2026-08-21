3c937b79964bc28f151a1072f7756fbc799ed9db
UD Centinela <ud.centinela@gmail.com>
2026-08-21 15:33:56 +0000

    revert(shield): restaurar escudos e imagenes originales

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 /**
  * UD Centinela - Service Worker PWA & Modo Estadio Offline
- * Version: 20260821_logo_v4
+ * Version: 20260821_revert_v1
  */
 
-const CACHE_NAME = 'udc-cache-v20260821_logo_v4';
-const DATA_CACHE_NAME = 'udc-data-cache-v20260821_logo_v4';
+const CACHE_NAME = 'udc-cache-v20260821_revert_v1';
+const DATA_CACHE_NAME = 'udc-data-cache-v20260821_revert_v1';
 
 const PRECACHE_ASSETS = [
   '/',
