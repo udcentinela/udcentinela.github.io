f22a57c720db429759a0e829abc0e578777a2398
UD Centinela <ud.centinela@gmail.com>
2026-08-22 10:23:02 +0000

    fix(players): eliminar fondo y recortar de barriga para arriba foto de Zacaria

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 /**
  * UD Centinela - Service Worker PWA & Modo Estadio Offline
- * Version: 20260822_champi_crop_v2
+ * Version: 20260822_zacaria_crop_v2
  */
 
-const CACHE_NAME = 'udc-cache-v20260822_champi_crop_v2';
-const DATA_CACHE_NAME = 'udc-data-cache-v20260822_champi_crop_v2';
+const CACHE_NAME = 'udc-cache-v20260822_zacaria_crop_v2';
+const DATA_CACHE_NAME = 'udc-data-cache-v20260822_zacaria_crop_v2';
 
 const PRECACHE_ASSETS = [
   '/',
